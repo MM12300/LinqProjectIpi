91ecdf525bfcbca3181131581248f92292edab89
Raphael Perles <progradius@protonmail.com>
2022-01-11 14:15:43 +0100

    menu

--- LinqProjectIpi/Program.cs
+++ LinqProjectIpi/Program.cs
@@ -13,5 +13,38 @@ namespace LinqProjectIpi
 
             Console.WriteLine("Welcome");
         }
+
+        private void menu(){
+            Console.Clear();
+            Console.WriteLine("-..-=-..-=-..-=-..-=-..-=-..-=-..-=-..-=-..-=-..-=-..-");
+            Console.WriteLine("                 Welcome to this Project");
+            Console.WriteLine("-..-=-..-=-..-=-..-=-..-=-..-=-..-=-..-=-..-=-..-=-..-");
+
+            Console.WriteLine("Choose your option:");
+            Console.WriteLine("1) Optionn 1");
+            Console.WriteLine("2) Option 2");
+            Console.WriteLine("3) Exit");
+
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("In Option 1");
+
+
+                    case "2":
+                        Console.WriteLine("In Option 2");
+
+
+                    case "3":
+                        Console.WriteLine("Exiting the program...");
+                        return null;
+
+                    default:
+                        Console.WriteLine("Bad input, exiting the game...");
+                        return null;
+                }
+            }
     }
 }
