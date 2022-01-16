d5bbf5c71aa6ce575e448a76c85ad24c5ee3da31
Matthieu M <nerggalkore@gmail.com>
2022-01-16 14:35:44 +0100

    Star Wars search by criterias (menu functions...)

--- LinqProjectIpi/StarWars/StarWarsCharactersHMI.cs
+++ LinqProjectIpi/StarWars/StarWarsCharactersHMI.cs
@@ -28,22 +28,35 @@ namespace LinqProjectIpi
         {
             Console.WriteLine("Choose an option :");
             Console.WriteLine("1 - All Characters Details");
+            Console.WriteLine("2 - Search Mode");
+            Console.WriteLine("3 - Return to main menu");
             Console.WriteLine();
             Console.WriteLine("\r\n Choose an option (1)");
 
             switch (Console.ReadLine())
                 {
-                    case "1":                        
-                        Console.WriteLine("Displays all Star Wars Characters Names");
-                        getAllCharactersDetails();
-                        main();
-                        break;
-                        
-                    default:
-                        Console.WriteLine("Choose from mentionned options only");
-                        Console.WriteLine("------------------------------------------");
-                        Console.WriteLine();
-                        break;
+                case "1":                        
+                    Console.WriteLine("Displays all Star Wars Characters Names");
+                    getAllCharactersDetails();
+                    main();
+                    break;
+
+                case "2":
+                    Console.WriteLine("Search characters with different criterias");
+                    searchMenu();
+                    main();
+                    break;
+
+                case "3":
+                    Console.WriteLine("Return to main menu");
+                    Console.WriteLine("To do");
+                    break;
+
+                default:
+                    Console.WriteLine("Choose from mentionned options only");
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine();
+                    break;
                 }
             }
 
@@ -64,6 +77,11 @@ namespace LinqProjectIpi
             IEnumerable<XElement> allcharacters = from characters in xmlFile.Descendants("character")
                                                   select characters;
 
+            characterOutput(allcharacters);
+        }
+
+        public void characterOutput(IEnumerable<XElement> allcharacters)
+        {
             foreach (XElement character in allcharacters)
             {
                 string output = "";
@@ -71,16 +89,26 @@ namespace LinqProjectIpi
                 {
                     if (element.Value != "none" && element.Value != "NA")
                     {
-                        output += element.Name + " : " + element.Value + "\r\n";
+                        if(element.Name == "Height")
+                        {
+                            output += element.Name + " : " + element.Value + "cm" + "\r\n";
+                        }
+                        else if(element.Name == "Mass")
+                        {
+                            output += element.Name + " : " + element.Value + "kg" + "\r\n";
+                        }
+                        else
+                        {
+                            output += element.Name + " : " + element.Value + "\r\n";
+                        }
+
                     }
                 }
                 output = cleanOutput(output);
                 Console.WriteLine(output);
                 Console.WriteLine("--");
             }
-            Console.ReadLine();
         }
-
 
         public string cleanOutput(string output)
         {
@@ -89,6 +117,130 @@ namespace LinqProjectIpi
             output = output.Replace("eye_color", "Eye Color");
             output = output.Replace("birth_year", "Birth Year");
             return output;
+        }
+
+        public void searchMenu()
+        {
+            Console.WriteLine("Search by :");
+            Console.WriteLine("1 - Name");
+            Console.WriteLine("2 - Height");
+            Console.WriteLine("3 - Mass");
+            Console.WriteLine("4 - Hair");
+            Console.WriteLine("5 - Skin");
+            Console.WriteLine("6 - Eye");
+            Console.WriteLine("7 - Birth Year");
+            Console.WriteLine("8 - Gender");
+            Console.WriteLine("9 - Homeworld");
+            Console.WriteLine("10 - Specie");
+            Console.WriteLine("11 - Return to Star Wars Menu");
+            Console.WriteLine();
+            Console.WriteLine("\r\n Choose an option)");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("Search characters by name");
+                    getCharactersBy("Name");
+                    searchMenu();
+                    break;
+                case "2":
+                    Console.WriteLine("Search characters by height");
+                    getCharactersBy("Height");
+                    searchMenu();
+                    break;
+                case "3":
+                    Console.WriteLine("Search characters by mass");
+                    getCharactersBy("Mass");
+                    searchMenu();
+                    break;
+                case "4":
+                    Console.WriteLine("Search characters by hair color");
+                    getCharactersBy("hair_color");
+                    searchMenu();
+                    break;
+                case "5":
+                    Console.WriteLine("Search characters by skin color");
+                    getCharactersBy("skin_color");
+                    searchMenu();
+                    break;
+                case "6":
+                    Console.WriteLine("Search characters by eye color");
+                    getCharactersBy("eye_color");
+                    searchMenu();
+                    break;
+                case "7":
+                    Console.WriteLine("Search characters by birth year");
+                    getCharactersBy("birth_year");
+                    searchMenu();
+                    break;
+                case "8":
+                    Console.WriteLine("Search characters by gender");
+                    getCharactersBy("Gender");
+                    searchMenu();
+                    break;
+                case "9":
+                    Console.WriteLine("Search characters by homeworld");
+                    getCharactersBy("Homeworld");
+                    searchMenu();
+                    break;
+                case "10":
+                    Console.WriteLine("Search characters by specie");
+                    getCharactersBy("Specie");
+                    searchMenu();
+                    break;
+                case "11":
+                    Console.WriteLine("'Return to main menu");
+                    main();
+                    break;
+
+                default:
+                    Console.WriteLine("Choose from mentionned options only");
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine();
+                    break;
+            }
+        }
+   
+
+        public void getCharactersBy(string criteria)
+        {
+            IEnumerable<XElement> characters = Enumerable.Empty<XElement>();
+            Console.WriteLine("What " + criteria + " would you like to search characters by ?");
+
+            if(criteria == "Gender")
+            {
+                Console.WriteLine("Filter by :");
+                Console.WriteLine("1 - male character(s)");
+                Console.WriteLine("2 - female character(s)");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        characters = from element in xmlFile.Descendants("character")
+                                         where element.Element("Gender").Value == "male"
+                                         select element;
+                        break;
+                    case "2":
+                        characters = from element in xmlFile.Descendants("character")
+                                         where element.Element("Gender").Value == "female"
+                                         select element;
+                        break;
+                    default:
+                        Console.WriteLine("Choose from mentionned options only");
+                        Console.WriteLine("------------------------------------------");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+            else
+            {
+                string search = Console.ReadLine();
+                characters = from element in xmlFile.Descendants("character")
+                                 where element.Element(criteria).Value.Contains(search, StringComparison.InvariantCultureIgnoreCase)
+                                 select element;
+            }
+
+            characterOutput(characters);
+            Console.ReadLine();
         }
     }
 }
